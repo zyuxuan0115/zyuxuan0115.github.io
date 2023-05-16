3d42fe46d5b421c093cb591e56061826addd0f67
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-05-16 19:56:57 -0400

    update

--- js/myjs.js
+++ js/myjs.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	fnc();
 });
 
-var l11a_counter = 0;
+var li1a_counter = 0;
 
 $(function (){
     $('#cat-pic').click(function(event){
@@ -26,7 +26,7 @@ $(function (){
     });
 
     $('#li1a').click(function(event){
-			 if ($(li1a_counter)%2 == 0) {
+			 if (li1a_counter%2 == 0) {
 				 $('#li1b').css('visibility', 'visible'); 
          $('#li1b').css('height', '100%');
 			 } 
