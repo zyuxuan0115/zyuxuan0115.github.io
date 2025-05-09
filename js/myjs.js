f5f9f59f7b698219a27d168193eb482d488a2f4f
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-05-09 14:19:51 -0400

    update animation

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,11 @@
 var textarea;
 
 var fnc=function(){
-	$("#expand-li1a").css('color', 'darkseagreen');	
-	$("#expand-li2a").css('color', 'darkseagreen');	
-	$("#expand-li3a").css('color', 'darkseagreen');	
-	$("#expand-liupenna").css('color', 'darkseagreen');	
-	$("#expand-liumicha").css('color', 'darkseagreen');	
+	$("#expand-li1a").css('color', 'darkseagreen');
+	$("#expand-li2a").css('color', 'darkseagreen');
+	$("#expand-li3a").css('color', 'darkseagreen');
+	$("#expand-liupenna").css('color', 'darkseagreen');
+	$("#expand-liumicha").css('color', 'darkseagreen');
 };
 
 $(document).ready(function(){
@@ -20,65 +20,53 @@ var liumicha_counter = 0;
 
 $(function (){
     $('#li1a').click(function(event){
-			 if (li1a_counter%2 == 0) {
-				 $('#li1b').css('visibility', 'visible'); 
-         $('#li1b').css('height', '100%');
-			 } 
-			 else {
-			   $('#li1b').css('visibility', 'hidden'); 
-         $('#li1b').css('height', '0px');
-			 }
-			 li1a_counter = li1a_counter+1;
+      if (li1a_counter%2 == 0) {
+         $('#li1b').stop(true, true).slideDown(300);
+      }
+      else {
+         $('#li1b').stop(true, true).slideUp(300);
+      }
+      li1a_counter = li1a_counter+1;
     });
 
     $('#li2a').click(function(event){
- 			 if (li2a_counter%2 == 0) {
-		     $('#li2b').css('visibility', 'visible'); 
-         $('#li2b').css('height', '100%'); 
-			 }
-			 else {
-			   $('#li2b').css('visibility', 'hidden'); 
-         $('#li2b').css('height', '0px');	
-			 }
-			 li2a_counter = li2a_counter + 1;
+      if (li2a_counter%2 == 0) {
+        $('#li2b').stop(true, true).slideDown(300);
+      }
+      else {
+        $('#li2b').stop(true, true).slideUp(300);
+      }
+      li2a_counter = li2a_counter + 1;
     });
 
     $('#li3a').click(function(event){
- 			 if (li3a_counter%2 == 0) {
-		     $('#li3b').css('visibility', 'visible'); 
-         $('#li3b').css('height', '100%'); 
-			 }
-			 else {
-			   $('#li3b').css('visibility', 'hidden'); 
-         $('#li3b').css('height', '0px');	
-			 }
-			 li3a_counter = li3a_counter + 1;
+      if (li3a_counter%2 == 0) {
+        $('#li3b').stop(true, true).slideDown(300);
+      }
+      else {
+        $('#li3b').stop(true, true).slideUp(300);
+      }
+      li3a_counter = li3a_counter + 1;
     });
 
-
-
     $('#li-upenn-a').click(function(event){
- 			 if (liupenna_counter%2 == 0) {
-		     $('#li-upenn-b').css('visibility', 'visible'); 
-         $('#li-upenn-b').css('height', '100%'); 
-			 }
-			 else {
-			   $('#li-upenn-b').css('visibility', 'hidden'); 
-         $('#li-upenn-b').css('height', '0px');	
-			 }
-			 liupenna_counter = liupenna_counter + 1;
+      if (liupenna_counter%2 == 0) {
+         $('#li-upenn-b').stop(true, true).slideDown(300);
+      }
+      else {
+        $('#li-upenn-b').stop(true, true).slideUp(300);
+      }
+      liupenna_counter = liupenna_counter + 1;
     });
 
     $('#li-umich-a').click(function(event){
- 			 if (liumicha_counter%2 == 0) {
-		     $('#li-umich-b').css('visibility', 'visible'); 
-         $('#li-umich-b').css('height', '100%'); 
-			 }
-			 else {
-			   $('#li-umich-b').css('visibility', 'hidden'); 
-         $('#li-umich-b').css('height', '0px');	
-			 }
-			 liumicha_counter = liumicha_counter + 1;
+      if (liumicha_counter%2 == 0) {
+         $('#li-umich-b').stop(true, true).slideDown(300);
+      }
+      else {
+         $('#li-umich-b').stop(true, true).slideUp(300);
+      }
+      liumicha_counter = liumicha_counter + 1;
     });
 
 
@@ -87,45 +75,42 @@ $(function (){
       $("#expand-li1a").css('color', 'white');
     });
 
-		$("#expand-li1a").mouseout(function(){
-			$("#expand-li1a").css('color', 'darkseagreen');	
-		});
-	
-		$("#expand-li2a").mouseover(function(){
-			$("#expand-li2a").css('color', 'white');
-		});
+    $("#expand-li1a").mouseout(function(){
+      $("#expand-li1a").css('color', 'darkseagreen');
+    });
 
-		$("#expand-li2a").mouseout(function(){
-			$("#expand-li2a").css('color', 'darkseagreen');	
-		});
+    $("#expand-li2a").mouseover(function(){
+      $("#expand-li2a").css('color', 'white');
+    });
 
-		$("#expand-li3a").mouseover(function(){
-			$("#expand-li3a").css('color', 'white');
-		});
+    $("#expand-li2a").mouseout(function(){
+      $("#expand-li2a").css('color', 'darkseagreen');
+    });
+    $("#expand-li3a").mouseover(function(){
+      $("#expand-li3a").css('color', 'white');
+    });
 
-		$("#expand-li3a").mouseout(function(){
-			$("#expand-li3a").css('color', 'darkseagreen');	
-		});
+    $("#expand-li3a").mouseout(function(){
+      $("#expand-li3a").css('color', 'darkseagreen');
+    });
 
-		$("#expand-liupenna").mouseover(function(){
-			$("#expand-liupenna").css('color', 'white');
-		});
+    $("#expand-liupenna").mouseover(function(){
+      $("#expand-liupenna").css('color', 'white');
+    });
 
-		$("#expand-liupenna").mouseout(function(){
-			$("#expand-liupenna").css('color', 'darkseagreen');	
-		});
-		
-		$("#expand-liumicha").mouseover(function(){
-			$("#expand-liumicha").css('color', 'white');
-		});
+    $("#expand-liupenna").mouseout(function(){
+      $("#expand-liupenna").css('color', 'darkseagreen');
+    });
 
-		$("#expand-liumicha").mouseout(function(){
-			$("#expand-liumicha").css('color', 'darkseagreen');	
-		});
-	
+    $("#expand-liumicha").mouseover(function(){
+      $("#expand-liumicha").css('color', 'white');
+    });
+
+    $("#expand-liumicha").mouseout(function(){
+      $("#expand-liumicha").css('color', 'darkseagreen');
+    });
+
     $('#cat-pic').mouseover(function(){
-       $('#cat-pic').css('cursor', "pic/cursor_write.gif"); 
+       $('#cat-pic').css('cursor', "pic/cursor_write.gif");
     });
 });
-
-
